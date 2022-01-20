5d862e2784af14efe39715fe535df8ed8d99b72c
sydneythompson <s38thomp@uwaterloo.ca>
2022-01-19 21:15:30 -0500

    completed constructor, destructor, and size function.

--- sequential-list.cpp
+++ sequential-list.cpp
@@ -1,27 +1,42 @@
 #include "sequential-list.h"
+using namespace std;
 
 SequentialList::SequentialList(unsigned int cap)
 {
+    capacity_ = cap;
+    DataType *list = new DataType [cap];  // sequential-list.h says int = DataType
+    data_ = list;
+    size_ = 0; // initialize at 0
 }
 
 SequentialList::~SequentialList()
 {
+    delete[ ] data_; // data_ and name of array (list)
 }
 
 unsigned int SequentialList::size() const
 {
+    int length = 0;
+    for( int i = 0; i < size_ ; i++); // go through every line and add one to size every time
+    {
+        length = length + 1;
+        return length;
+    }
 }
 
 unsigned int SequentialList::capacity() const
 {
+    return capacity_;
 }
 
 bool SequentialList::empty() const
 {
+    // if else
 }
 
 bool SequentialList::full() const
 {
+    // if else
 }
 
 SequentialList::DataType SequentialList::select(unsigned int index) const
